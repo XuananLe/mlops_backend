671a650a437f2ab98074ce782eee7a3e426204ff
nvquynh29 <nvquynh29@gmail.com>
2023-03-20 17:44:21 +0700

    chore: refactor services

--- src/api/v1/controllers/project.controller.js
+++ src/api/v1/controllers/project.controller.js
@@ -1,9 +1,10 @@
 import ProjectService from '../services/project.service.js'
 
 const Create = async (req, res) => {
+  const { _id } = req.user
   try {
-    const project = await ProjectService.Create(req)
-    return res.json({ data: project })
+    const project = await ProjectService.Create(_id, req.body)
+    return res.json(project)
   } catch (error) {
     console.error(error)
     return res.status(500).json({ error: 'Internal Server Error' })
@@ -11,8 +12,11 @@ const Create = async (req, res) => {
 }
 
 const UploadFiles = async (req, res) => {
+  const { _id } = req.user
+  const { id } = req.params
+  const { type } = req.body
   try {
-    const uploadedFiles = await ProjectService.UploadFiles(req)
+    const uploadedFiles = await ProjectService.UploadFiles(_id, id, req.files.files, type)
     return res.json(uploadedFiles)
   } catch (error) {
     console.error(error)
