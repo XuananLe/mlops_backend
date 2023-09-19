9154bc546b4ac4c73dfcb2cc2845db158a352446
xuananle <lexuanan18102004@gmail.com>
2023-09-19 19:50:34 +0700

    Adding guide to Storage API

--- buckets.cjs
+++ buckets.cjs
@@ -16,4 +16,42 @@ async function listBuckets() {
 }
 
 listBuckets();
-                                                                                                                                                             
+
+// list all object in bucket
+async function listFiles() {
+  try {
+    const [files] = await storage.bucket("nas-rd320-model").getFiles();
+
+    console.log('Files:');
+    files.forEach((file) => {
+      console.log(file.name);
+    });
+  } catch (err) {
+    console.error('ERROR:', err);
+  }
+}
+listFiles();
+
+
+async function uploadFile(filename = "test.txt", bucketName = "nas-rd320-model") {
+  try {
+    await storage.bucket("nas-rd320-model").upload("./test.txt", {
+      // Support for HTTP requests made with `Accept-Encoding: gzip`
+      gzip: true,
+      // By setting the option `destination`, you can change the name of the
+      // object you are uploading to a bucket.
+      metadata: {
+        // Enable long-lived HTTP caching headers
+        // Use only if the contents of the file will never change
+        // (If the contents will change, use cacheControl: 'no-cache')
+        cacheControl: 'public, max-age=31536000',
+      },
+    });
+
+    console.log(`${filename} uploaded to ${bucketName}.`);
+  } catch (err) {
+    console.error('ERROR:', err);
+  }
+}
+uploadFile();
+
