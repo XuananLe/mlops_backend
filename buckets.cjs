af8282e56f4e0bfc28179894f6c901b94c5c4213
xuananle <lexuanan18102004@gmail.com>
2023-09-19 10:50:07 +0700

    Example Cloud API

---
+++ buckets.cjs
@@ -0,0 +1,19 @@
+const { Storage } = require('@google-cloud/storage');
+
+const storage = new Storage();
+
+async function listBuckets() {
+  try {
+    const [buckets] = await storage.getBuckets();
+
+    console.log('Buckets:');
+    buckets.forEach((bucket) => {
+      console.log(bucket.name);
+    });
+  } catch (err) {
+    console.error('Error listing buckets:', err);
+  }
+}
+
+listBuckets();
+                                                                                                                                                             
